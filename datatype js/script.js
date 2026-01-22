736c6a5f05538920b031f45822925cf929d82fed
Nidhi Rathor <nidhirathor6260@gmail.com>
2026-01-22 10:18:36 +0530

    javaScript datatypes

---
+++ datatype js/script.js
@@ -0,0 +1,24 @@
+console.log("Hey I am Nidhi")
+
+let a = 5;
+let b = 6;
+let c = "Nidhi"
+console.log(a+b);
+console.log(typeof a , typeof b, typeof c)
+
+{
+    let a = 65;
+}
+
+console.log(a);
+
+let primitive_datatype = ["null" , "number", "string", "boolean", "undefined", "bigInt", "symbol"]
+console.log(primitive_datatype);
+
+let object = {
+    name: "Nidhi",
+   "Admission number": "25b01010725"
+}
+
+object.course = "B.tech";
+console.log(object);
